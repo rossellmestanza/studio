40569c7481d6dc45aa2c2eb1093af68187bec753
Rossell Mestanza <Rossell.Mestanza@gmail.com>
2025-11-23 06:28:12 +0000

    studio-1337196521:~/studio{main}$ npm run dev
    > nextn@0.1.0 dev
    > next

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,7 @@
 export const firebaseConfig = {
   "projectId": "studio-1337196521-82ace",
   "appId": "1:515437915886:web:80b518f7ae6a6ed8de4dff",
-  "storageBucket": "studio-1337196521-82ace.appspot.com",
+  "storageBucket": "studio-1337196521-82ace.firebasestorage.app",
   "apiKey": "AIzaSyDF8j3MpwG3AdDQJ3HNa1rUh-MvZk-IE1g",
   "authDomain": "studio-1337196521-82ace.firebaseapp.com",
   "measurementId": "",
